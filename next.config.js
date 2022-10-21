e590b2a704c127f20fc26a3da4547ae0fd9dbecc
DataRohit <ingobingo3491@gmail.com>
2022-10-21 13:36:01 +0530

    next config fixed

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
 
-module.exports = nextConfig
+  env: {
+    NEXT_PUBLIC_GOOGLE_ID: process.env.NEXT_PUBLIC_GOOGLE_ID,
+    NEXT_PUBLIC_GOOGLE_SECRET: process.env.NEXT_PUBLIC_GOOGLE_SECRET,
+    NEXT_PUBLIC_NEXTAUTH_URL: process.env.NEXT_PUBLIC_NEXTAUTH_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+  },
+};
+
+module.exports = nextConfig;
